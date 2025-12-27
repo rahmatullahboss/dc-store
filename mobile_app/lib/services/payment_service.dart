9582cddf018123e65b402a86b4f40d224a06ce36
rahmatullahboss <rahmatullahzisan01@gmail.com>
2025-12-27 14:34:57 +0600

    ci: Update Flutter workflow to include new checks or dependency updates.

--- mobile_app/lib/services/payment_service.dart
+++ mobile_app/lib/services/payment_service.dart
@@ -1,5 +1,4 @@
 import 'dart:convert';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_stripe/flutter_stripe.dart';
 import 'package:http/http.dart' as http;
