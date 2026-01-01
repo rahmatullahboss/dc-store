53a234f4df27096787faee0cbda4ab5d477dcbb8
rahmatullahboss <rahmatullahzisan01@gmail.com>
2026-01-01 20:31:47 +0600

    fix: Use locale-aware Link in admin sidebar

--- src/components/admin/admin-sidebar.tsx
+++ src/components/admin/admin-sidebar.tsx
@@ -1,7 +1,6 @@
 "use client";
 
-import Link from "next/link";
-import { usePathname } from "next/navigation";
+import { Link, usePathname } from "@/i18n/routing";
 import { cn } from "@/lib/utils";
 import {
   LayoutDashboard,
