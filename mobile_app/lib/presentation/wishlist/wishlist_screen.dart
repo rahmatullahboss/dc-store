7ba19e3a0ca04cb2d9d848d952b3eadb460ecd30
rahmatullahboss <rahmatullahzisan01@gmail.com>
2026-01-01 10:03:56 +0600

    fix(flutter): fix _WishlistItem uninitialized fields

--- mobile_app/lib/presentation/wishlist/wishlist_screen.dart
+++ mobile_app/lib/presentation/wishlist/wishlist_screen.dart
@@ -670,5 +670,8 @@ class _WishlistItem {
     required this.name,
     required this.price,
     required this.imageUrl,
+    this.originalPrice,
+    this.isInStock = true,
+    this.discountPercent,
   });
 }
